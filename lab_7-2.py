a125e8c04fd2b923b40db21d93d7e9343d0fb73b
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-12-07 12:26:01 -0500

    done 7-1 and 7-2, wip 7-3

---
+++ lab_7-2.py
@@ -0,0 +1,9 @@
+# Creator JM 12/7/22
+
+def find_sum(num1, num2):
+    num_sum = num1 + num2
+    return num_sum
+
+my_sum = find_sum(111,222)
+
+# Nothing happens because we never added a print statement!
---
+++ lab_7-3.py
@@ -0,0 +1,8 @@
+# Creator JM 12/7/22
+
+def greeting():
+    ''' This will print hello world on 1 line, that all'''
+    print('Hello, World!')
+    return help(greeting)
+
+print(greeting() == help((greeting)))
